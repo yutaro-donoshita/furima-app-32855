3a86d77758ed8935cdbc78e115684e22096b053f
yutaro-donoshita <uver.gp.c@softbank.ne.jp>
2021-05-12 18:48:32 +0900

    一覧表示画面の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,15 +3,15 @@ class ItemsController < ApplicationController
   before_action :authenticate_user! ,only: [:new,:create] 
 
   def index
-    @items=Item.includes(:user).order("created_at DESC")
+    @items=Item.includes(:user).order('created_at DESC')
   end
   
   def new
-     @item=Item.new
+     @item = Item.new
    end
 
    def create
-     @item=Item.new(item_params)
+     @item = Item.new(item_params)
      if @item.save
        redirect_to root_path
      else

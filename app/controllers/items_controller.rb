9da72e5482b9cabb1d73b396942e36e4e9694cfd
yutaro-donoshita <uver.gp.c@softbank.ne.jp>
2021-05-12 10:27:09 +0900

    コードレビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,11 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user! ,only: [:new,:create] 
+
   def index
   end
   
   def new
-     unless user_signed_in?
-       redirect_to new_user_session_path
-     end
      @item=Item.new
    end
 

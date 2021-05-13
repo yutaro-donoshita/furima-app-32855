35a010084c64e4c223419ef4f53cc7c3559fde6a
yutaro-donoshita <uver.gp.c@softbank.ne.jp>
2021-05-13 20:29:17 +0900

    item#showルーティング記述とコントローラーにshowメソッド記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,19 +8,23 @@ class ItemsController < ApplicationController
   
   def new
      @item = Item.new
-   end
+  end
 
-   def create
-     @item = Item.new(item_params)
-     if @item.save
-       redirect_to root_path
-     else
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
        render :new
-     end
-   end
+    end
+  end
 
-   private
+  def show
+    @item=Item.find(params[:id])
+  end
+
+  private
    def item_params
      params.require(:item).permit(:item_name,:item_text,:category_id,:status_id,:delivery_fee_id,:prefecture_id,:shipment_date_id,:price,:item_image).merge(user_id:current_user.id)
    end
- end
+end

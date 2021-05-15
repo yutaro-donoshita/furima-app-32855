a4302cc014e2b38db82d79cc2ebcf8b034dc6fd9
yutaro-donoshita <uver.gp.c@softbank.ne.jp>
2021-05-15 17:04:09 +0900

    commitし忘れ。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update,]
   before_action :set_item, only: [:edit,:update,:show]
-  before_action :move_to_index , only: [:edit,:update
+  before_action :move_to_index , only: [:edit,:update]
 
   def index
    @items = Item.order('created_at DESC')

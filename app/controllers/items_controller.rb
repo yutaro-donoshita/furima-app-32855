589c017e873c750361b6cc57783e3b717d67e990
yutaro-donoshita <uver.gp.c@softbank.ne.jp>
2021-05-15 19:46:59 +0900

    コードレビュー修正 #2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-      redirect_to item_path(@item.id)
+      redirect_to item_path(item.id)
     else
       render :edit
     end

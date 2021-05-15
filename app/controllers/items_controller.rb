f2748463afa3aa5f31e4829f66716e09a69c1f8f
yutaro-donoshita <uver.gp.c@softbank.ne.jp>
2021-05-15 16:51:50 +0900

    コード修正　#7

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update,]
   before_action :set_item, only: [:edit,:update,:show]
-
+  before_action :move_to_index , only: [:edit,:update
 
   def index
    @items = Item.order('created_at DESC')
@@ -42,5 +42,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id != @item.user_id
   end
 end
